9d9efb4666a2cc13b71513416a986a4a2ad68335
kristitolp <kristi.tolp@tktk.ee>
2024-04-23 18:13:26 +0300

    add week2 code

---
+++ UserAgeValidation2/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Rakendus käsib kasutajal sisestada vanuse
+//Kui kasutaja sisestab vanuse noorema kui 13, siis kuvatakse konsoolis:
+//"You are too young to use Instagram"
+//muul juhul
+//Konsoolis kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+//boolean = true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//int userAge = Int32.Parse(Console.ReadLine()); //"13"-heap,13-stack
+
+Console.WriteLine($"Parse result{isAgeNumber} . User is {userAgeNum} years old.");
+
+
+if (isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+
+    else
+    {
+        Console.WriteLine("You are too young to use instagram!");
+    }
+
+
+}
+
+else
+{
+    Console.WriteLine("Could not read your age");
+}
+
+
+
+//if (userAge >= 13)
+
+// Console.WriteLine("Welcome to Instagram!");
+
+//else //if (userAge<13)
+
+//Console.WriteLine("You are too young to use instagram!");
---
+++ Week2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Rakendus küsib kasutaja sugu
+//Ranekdus küsib kasutaja perekonnanime
+// Rakendus tervitab kasutajat järgmiselt:
+// "Welcome Mr. [kasutaja perekonnanimi]" / "welcome, Ms. [kasutaja perekonnanimi]"
+Console.WriteLine("Please select your gender(m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb consoolist andmeid string ehk sõne formaadis
+
+Console.WriteLine("Please Enter your lastname");
+
+string userLastName = Console.ReadLine();
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName} !");
+}
+
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName} !");
+}
+else
+{
+    Console.WriteLine($"Welcome {userLastName} ! ");
+}   
